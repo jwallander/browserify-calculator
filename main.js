efe7115c698dac5ffebd96d078449344e18d4f06
matthewbeta <hello@matthew-jackson.com>
2014-07-02 21:26:56 +0100

    Adding the basic maths ops and using them to log out some simple sums

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var $ = require('jquery');
-var _ = require('underscore');
+var add = require('./modules/add.js');
+var subtract = require('./modules/subtract.js');
+var multiply = require('./modules/multiply.js');
+var divide = require('./modules/divide.js');
 
-var arr = [1,2,3,4,5,6,7,8,9,0];
-
-var evens = _.filter(arr, function(num){
-  return num % 2 === 0;
-});
-
-console.log(evens);
+console.log("1 + 2 = " + add(1,2)); // 3
+console.log("3 - 1 = " + subtract(3,1)); // 2
+console.log("3 x 4 = " + multiply(3,4)); // 12
+console.log("12 ÷ 3 = " + divide(3,12)); // 4
