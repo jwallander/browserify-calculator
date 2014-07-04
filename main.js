cd6c7c61f58af489a913b54b7fba860e907b5790
matthewbeta <hello@matthew-jackson.com>
2014-07-04 20:25:53 +0100

    Stage 5 Handle the number buttons
    - commented out the console log demos
    - using higher order fn to to update the display

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+var $ = require('jquery');
+
 var add = require('./modules/add.js');
 var subtract = require('./modules/subtract.js');
 var multiply = require('./modules/multiply.js');
 var divide = require('./modules/divide.js');
 var output = require('./modules/output.js');
+var numberBtns = require('./modules/number-btns.js');
 
-console.log("1 + 2 = " + add(1,2)); // 3
-console.log("3 - 1 = " + subtract(3,1)); // 2
-console.log("3 x 4 = " + multiply(3,4)); // 12
-console.log("12 ÷ 3 = " + divide(3,12)); // 4
+var outputEl = document.getElementById('output');
 
-output('#output', 'Hello');
+// console.log("1 + 2 = " + add(1,2)); // 3
+// console.log("3 - 1 = " + subtract(3,1)); // 2
+// console.log("3 x 4 = " + multiply(3,4)); // 12
+// console.log("12 ÷ 3 = " + divide(3,12)); // 4
+
+// output('#output', 'Hello');
+
+$('#numbers').on('click', 'input', function() {
+
+	output(outputEl, numberBtns(this, outputEl));
+
+});
